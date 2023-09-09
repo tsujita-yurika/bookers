1f31f2089bc2e19705d7a32532ead96adf35c619
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-09-09 03:25:33 +0000

    [Update]3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,25 @@
 class BooksController < ApplicationController
+  
   def index
+    @Book = Book.all
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
+    
   def show
   end
 
   def edit
   end
+  
+  private
+  
+  def  book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
+  post 'books' => 'books#create' 
   get 'books/show'
   get 'books/edit'
   get '/' => 'homes#top'
